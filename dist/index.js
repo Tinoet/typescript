2d494541c06b6279afc90af5b3f07c3606d58ab2
Tinoet <elijahtinoet@gmail.com>
2022-10-28 01:07:10 +0000

    first commit

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,2 @@
 "use strict";
-// command lines: npm i -g typescript, tsc -v, tsc --init, tsc
-// "target": "ES2016", 
-// "module": "commonjs",                                /* Specify what module code is generated. */
-// "rootDir": "./src", 
-// debuging typescript application: go to emit section: go to the source map(enable it):specifies ho eachh line of our tpescript code maps to our generated js code 
 let age = 20;
-
